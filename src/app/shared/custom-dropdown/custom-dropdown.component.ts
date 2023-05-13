85c5e959950b453c03c0571b64cc4cd5876d2dab
Modzou <mdmodzou@gmail.com>
2023-05-14 01:27:42 +0200

    design appli

--- src/app/shared/custom-dropdown/custom-dropdown.component.ts
+++ src/app/shared/custom-dropdown/custom-dropdown.component.ts
@@ -1,19 +1,47 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+interface ImageOption {
+  imagePath: string;
+  altText: string;
+}
 @Component({
   selector: 'app-custom-dropdown',
   templateUrl: './custom-dropdown.component.html',
   styleUrls: ['./custom-dropdown.component.scss']
 })
-export class CustomDropdownComponent {
-  selectedOption!: string;
-  options: any[] = [
-    { value: 'option1', label: 'Option 1', image: 'assets/images/logo.jpeg' },
-    { value: 'option2', label: 'Option 2', image: 'assets/images/logo.jpeg' },
+export class CustomDropdownComponent implements OnInit {
+  selectedImage: string = ''; // Stores the selected image path
+  selectedImageAlt: string = '';
+  imageOptions: ImageOption[] = [
+    {
+      altText: 'Wave',
+      imagePath: '../../../assets/images/wave.png'
+    },
+    {
+      altText: 'Orange money',
+      imagePath: '../../../assets/images/om.png'
+    },
+    {
+      altText: 'Free Money',
+      imagePath: '../../../assets/images/free.png'
+    }
   ];
 
-  onOptionChange() {
-    // Perform any desired action based on the selected value
-    console.log(this.selectedOption);
+  ngOnInit(): void {
+    // Set default image based on your condition or logic
+    this.setDefaultImage();
+  }
+
+  setDefaultImage(): void {
+    // Your condition or logic to determine the default image
+    // Here's an example of setting the first image as the default
+    if (this.imageOptions.length > 0) {
+      this.selectedImage = this.imageOptions[0].imagePath;
+      this.selectedImageAlt = this.imageOptions[0].altText;
+    }
+  }
+
+  onSelectImage(option: ImageOption): void {
+    this.selectedImage = option.imagePath; // Update the selected image path
+    this.selectedImageAlt = option.altText;
   }
 }
